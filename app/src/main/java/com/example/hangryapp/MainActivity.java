7672487fcfabae49e8b3925a9749b3fe654227df
Nicole Feder <federn@umich.edu>
2019-11-12 13:52:45 -0500

    nicole feder test

--- app/src/main/java/com/example/hangryapp/MainActivity.java
+++ app/src/main/java/com/example/hangryapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //nicole feder push TEST
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
