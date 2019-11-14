0f15234dd6dbfc03489c6da41f383b30442f1888
Nicole Feder <federn@umich.edu>
2019-11-14 13:47:35 -0500

    User Authentication first step

--- app/src/main/java/com/example/hangryapp/MainActivity.java
+++ app/src/main/java/com/example/hangryapp/MainActivity.java
@@ -3,20 +3,42 @@ package com.example.hangryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //nicole feder push TEST
-    //jordyn push test
-    //Lisa push test
-    //Aish push TEST
-    //peter push test
-    // Chris push test :'[
+
+    //Step 1
+    EditText editTextEmail, editTextPassword;
+    Button buttonLogin, buttonRegister;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Step 2
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        //Step 3
+        buttonLogin.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
     }
 }
+
+
+
