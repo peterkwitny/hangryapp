04b19f0fc689707f5b80a20503134e95cc0129c7
asladdha <asladdha@umich.edu>
2019-11-19 13:54:19 -0500

    aish pushing meal class

--- app/src/main/java/Meal.java
+++ app/src/main/java/Meal.java
@@ -1,2 +1,27 @@
 public class Meal {
+
+    public String name, restaurant;
+    public String[] listOfFoodTraits;
+    public double price;
+    public boolean vegan, glutenFree, vegetarian, dairyFree, nutFree;
+
+    public Meal(){
+
+    }
+
+    public Meal(String mealName, String restaurantName, String[] foodTraits, int numberOfFoodTraits, double foodPrice,
+                boolean vegan, boolean glutenFree, boolean vegetarian, boolean dairyFree, boolean nutFree){
+
+        this.name = mealName;
+        this.restaurant = restaurantName;
+        this.listOfFoodTraits = new String[numberOfFoodTraits];
+        this.listOfFoodTraits = foodTraits;
+        this.price = foodPrice;
+        this.vegan = vegan;
+        this.glutenFree = glutenFree;
+        this.vegetarian = vegetarian;
+        this.dairyFree = dairyFree;
+        this.nutFree = nutFree;
+    }
+
 }
