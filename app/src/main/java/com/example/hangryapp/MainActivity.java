e2be1ad858475c94df0010a7e6fcc4c17435bbb6
Nicole Feder <federn@umich.edu>
2019-11-19 13:14:01 -0500

    User authentication done

--- app/src/main/java/com/example/hangryapp/MainActivity.java
+++ app/src/main/java/com/example/hangryapp/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.hangryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
 
     EditText editTextEmail, editTextPassword;
     Button buttonLogin, buttonRegister;
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -27,16 +35,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonLogin.setOnClickListener(this);
         buttonRegister.setOnClickListener(this);
-    }
 
+        mAuth = FirebaseAuth.getInstance();
+
+    }
 
     @Override
     public void onClick(View v) {
-        String email = editTextEmail.getText().toString();
-        String password = editTextPassword.getText().toString();
+
+        // Intent mainIntent = new Intent(this, PortalActivity.class);
+        //  startActivity(mainIntent);
+
+        if (v == buttonRegister){
+            makeNewUsers(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+
+        } else if(v == buttonLogin){
+            loginUser(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+
+        }
 
     }
+
+    public void makeNewUsers(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //Display a success message
+                            Toast.makeText(MainActivity.this, "User Registration Successful", Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "User Registration Failed", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
 }
-
-
-
