935afb1d8ea0aae8849a5b584ebdaa163c3b35a6
peterkwitny <55850542+peterkwitny@users.noreply.github.com>
2019-11-21 14:02:57 -0500

    test

--- app/src/main/java/Meal.java
+++ app/src/main/java/Meal.java
@@ -1,27 +1,23 @@
 public class Meal {
 
     public String name, restaurant;
-    public String[] listOfFoodTraits;
+    public String mealtime, cuisine;
     public double price;
     public boolean vegan, glutenFree, vegetarian, dairyFree, nutFree;
 
-    public Meal(){
-
+    public Meal() {
     }
 
-    public Meal(String mealName, String restaurantName, String[] foodTraits, int numberOfFoodTraits, double foodPrice,
-                boolean vegan, boolean glutenFree, boolean vegetarian, boolean dairyFree, boolean nutFree){
-
-        this.name = mealName;
-        this.restaurant = restaurantName;
-        this.listOfFoodTraits = new String[numberOfFoodTraits];
-        this.listOfFoodTraits = foodTraits;
-        this.price = foodPrice;
+    public Meal(String name, String restaurant, String trait1, String trait2, double price, boolean vegan, boolean glutenFree, boolean vegetarian, boolean dairyFree, boolean nutFree) {
+        this.name = name;
+        this.restaurant = restaurant;
+        this.mealtime = trait1;
+        this.cuisine = trait2;
+        this.price = price;
         this.vegan = vegan;
         this.glutenFree = glutenFree;
         this.vegetarian = vegetarian;
         this.dairyFree = dairyFree;
         this.nutFree = nutFree;
     }
-
 }
