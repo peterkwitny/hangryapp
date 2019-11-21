e02b3443c47a9c3c67b08d695f2a244161718408
Nicole Feder <federn@umich.edu>
2019-11-21 00:14:05 -0500

    Home screen and log in page complete with firebase authentication

--- app/src/main/java/com/example/hangryapp/MainActivity.java
+++ app/src/main/java/com/example/hangryapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.hangryapp;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -9,21 +9,14 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText editTextEmail, editTextPassword;
+
     Button buttonLogin, buttonRegister;
 
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,71 +30,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         animationDrawable.start();
 
 
-
-//        editTextEmail = findViewById(R.id.editTextEmail);
-//        editTextPassword = findViewById(R.id.editTextPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
         buttonRegister = findViewById(R.id.buttonRegister);
 
         buttonLogin.setOnClickListener(this);
         buttonRegister.setOnClickListener(this);
 
-//        mAuth = FirebaseAuth.getInstance();
+
 
     }
 
     @Override
     public void onClick(View v) {
 
-        // Intent mainIntent = new Intent(this, PortalActivity.class);
-        //  startActivity(mainIntent);
 
-        if (v == buttonRegister){
-//            makeNewUsers(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+        if (v == buttonLogin){
+            Intent mainIntent = new Intent(this, LoginActivity.class);
+            startActivity(mainIntent);
 
-        } else if(v == buttonLogin){
-//            loginUser(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+        } else if(v == buttonRegister){
+            Intent mainIntent = new Intent(this, RegisterActivity.class);
+            startActivity(mainIntent);
 
         }
 
     }
 
-//    public void makeNewUsers(String email, String password){
-//        mAuth.createUserWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            //Display a success message
-//                            Toast.makeText(MainActivity.this, "User Registration Successful", Toast.LENGTH_SHORT).show();
-//
-//
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Toast.makeText(MainActivity.this, "User Registration Failed", Toast.LENGTH_SHORT).show();
-//
-//                        }
-//
-//                        // ...
-//                    }
-//                });
-//    }
-
-//    public void loginUser(String email, String password) {
-//        mAuth.signInWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//
-//
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-//                        }
-//
-//                        // ...
-//                    }
-//                });
-//    }
 }
